3014a01b4dad6870a516d1746191e6b06159edc9
Harsha Vardhan Reddy Bagili <110410067+HarshaVardhanReddyBagili@users.noreply.github.com>
2023-02-07 22:01:28 +0530

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -14,7 +14,7 @@ uploaded_file = st.file_uploader(" ", type=['xlsx'])
 if uploaded_file is not None:     
     cement = pd.read_excel(uploaded_file)
     cement['Month'] = cement['Month'].apply(lambda x: x.strftime('%B-%Y'))
-    st.write("Plese wait... Model is working on it")
+    st.write("Plese wait for the forecasting result... Model is working on it")
     
     mod = AutoTS(forecast_length=12, frequency='M', prediction_interval = 0.90,
              ensemble= None, model_list = 'univariate', max_generations = 3, num_validations= 2,
