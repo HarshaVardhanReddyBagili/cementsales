ceb206ae094bf4efe09579678c28f3b09993d0a1
Harsha Vardhan Reddy Bagili <110410067+HarshaVardhanReddyBagili@users.noreply.github.com>
2023-02-07 22:00:37 +0530

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -2,10 +2,10 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-from statsmodels.tsa.seasonal import seasonal_decompose
-from statsmodels.tsa.holtwinters import SimpleExpSmoothing # SES
-from statsmodels.tsa.holtwinters import Holt # Holts Exponential Smoothing
-from statsmodels.tsa.holtwinters import ExponentialSmoothing
+#from statsmodels.tsa.seasonal import seasonal_decompose
+#from statsmodels.tsa.holtwinters import SimpleExpSmoothing # SES
+#from statsmodels.tsa.holtwinters import Holt # Holts Exponential Smoothing
+#from statsmodels.tsa.holtwinters import ExponentialSmoothing
 from autots import AutoTS
 
 st.title('Forecasting the Cement Sales')
@@ -14,6 +14,7 @@ uploaded_file = st.file_uploader(" ", type=['xlsx'])
 if uploaded_file is not None:     
     cement = pd.read_excel(uploaded_file)
     cement['Month'] = cement['Month'].apply(lambda x: x.strftime('%B-%Y'))
+    st.write("Plese wait... Model is working on it")
     
     mod = AutoTS(forecast_length=12, frequency='M', prediction_interval = 0.90,
              ensemble= None, model_list = 'univariate', max_generations = 3, num_validations= 2,
@@ -24,13 +25,7 @@ if uploaded_file is not None:
     
     forecast = prediction.forecast
     
-    #hwe_model_mul_add = ExponentialSmoothing(cement["Sales"][:71], seasonal = "mul", trend = "add", seasonal_periods = 12).fit()
-    
-    #newdata_pred = hwe_model_mul_add.predict(start = cement.index[0], end = cement.index[-1])
-    
-    st.subheader("For exponential model")
+    st.subheader("From AutoTS model")
    
-    st.write("Sales Forecast: ", forecast)
-   
-    
-    st.subheader("Thanks for visit.")
+    st.write("Forecast of Sales for next 12 months: ", forecast)
+  
