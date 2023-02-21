a98834d51aa52387a4706f509d9ace5b7387e929
Harsha Vardhan Reddy Bagili <110410067+HarshaVardhanReddyBagili@users.noreply.github.com>
2023-02-21 18:16:44 +0530

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -10,27 +10,28 @@ html_temp = """
 <h2 style="color:white;text-align:center;">Forecasting The Cement Sales </h2>
 </div>
 """
-st.markdown(html_temp, unsafe_allow_html = True)
-st.text("")
-uploaded_file = st.file_uploader(" ", type=['xlsx'])
+if st.button("Predict"):
+    st.markdown(html_temp, unsafe_allow_html = True)
+    st.text("")
+    uploaded_file = st.file_uploader(" ", type=['xlsx'])
 
-if uploaded_file is not None:     
-    cement = pd.read_excel(uploaded_file)
-    cement['Month'] = cement['Month'].apply(lambda x: x.strftime('%B-%Y'))
+    if uploaded_file is not None:
+        cement = pd.read_excel(uploaded_file)
+        cement['Month'] = cement['Month'].apply(lambda x: x.strftime('%B-%Y'))
 
-    st.write("Plese wait for the forecasting result... Model is working on it")
+        st.write("Plese wait for the forecasting result... Model is working on it")
     
-    mod = AutoTS(forecast_length=12, frequency='M', prediction_interval = 0.90,
-             ensemble= None, model_list = 'superfast', max_generations = 4, num_validations= 2,
-             no_negatives = True,n_jobs = 'auto')
+        mod = AutoTS(forecast_length=12, frequency='M', prediction_interval = 0.90,
+                     ensemble= None, model_list = 'superfast', max_generations = 4, num_validations= 2,
+                     no_negatives = True,n_jobs = 'auto')
 
-    mod = mod.fit(cement, date_col='Month', value_col='Sales')
-    prediction = mod.predict()
+        mod = mod.fit(cement, date_col='Month', value_col='Sales')
+        prediction = mod.predict()
     
-    forecast = prediction.forecast
-    st.subheader("Here we have the sales for next 12 months:")
-    cm = sns.light_palette("teal", as_cmap=True)
-    st.table(forecast.style.background_gradient(cmap=cm).set_precision(2))
+        forecast = prediction.forecast
+        st.subheader("Here we have the sales for next 12 months:")
+        cm = sns.light_palette("teal", as_cmap=True)
+        st.table(forecast.style.background_gradient(cmap=cm).set_precision(2))
     
     
 
